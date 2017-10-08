129e5bbbb4d5262b6a635c45a9861d2b252448b4
Michael Tauer <michael.tauer@futurice.com>
2017-10-08 20:55:00 +0200

    Add skeleton for stream edit page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
+import StreamEditPage from './features/streamEdit/StreamEditPage';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <h1>Rx Cards</h1>
+        <StreamEditPage />
       </Provider>
     );
   }
---
+++ src/features/streamEdit/StreamEditPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StreamEditPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>StreamEditPage</h1>
+      </div>
+    );
+  }
+}
+
+export default StreamEditPage;
