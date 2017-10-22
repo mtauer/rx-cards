f0c9ebec74285465f13c6fd3c0bd5366a76e906b
Michael Tauer <michael.tauer@futurice.com>
2017-10-22 08:24:16 +0200

    Update stream messages when operator options are changed

--- src/features/streamEdit/redux.js
+++ src/features/streamEdit/redux.js
@@ -1,4 +1,4 @@
-import Rx from 'rxjs/Rx';
+import 'rxjs';
 import { Notification } from 'rxjs/Notification';
 import { createSelector } from 'reselect';
 import _ from 'lodash';
@@ -32,7 +32,7 @@ const initialState = {
   definedStreamsMessages: {
     input: [
       { frame: 40, notification: Notification.createNext(1) },
-      { frame: 60, notification: Notification.createNext(2) },
+      { frame: 63, notification: Notification.createNext(2) },
       { frame: 80, notification: Notification.createNext(3) },
       { frame: 150, notification: Notification.createNext(4) },
       { frame: 280, notification: Notification.createNext(5) },
@@ -121,12 +121,13 @@ export default function streamEditReducer(state = initialState, action) {
 // Epics
 
 export function createSimulateMessagesEpic() {
-  return (action$, store) =>
-    Rx.Observable.of(null)
-      .map(() => {
-        const streamsMessages = getStreamsMessages(store.getState());
-        return setStreamsMessagesAction(streamsMessages);
-      });
+  return (action$, store) => action$
+    .filter(action => action.type === SET_OPERATOR_OPTIONS)
+    .startWith(null)
+    .map(() => {
+      const streamsMessages = getStreamsMessages(store.getState());
+      return setStreamsMessagesAction(streamsMessages);
+    });
 }
 
 // Selectors
